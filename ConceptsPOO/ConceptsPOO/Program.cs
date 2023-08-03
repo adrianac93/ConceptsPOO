9c1e61caac46f41b8c71f3b77f9af95b83422199
adriana maestre <adrianalamasbonita_15@hotmail.com>
2023-08-02 21:27:43 -0400

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(2009, 9, 23));
+	Console.WriteLine(new Date(2008, 11, 31));
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
