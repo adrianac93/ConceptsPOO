83904cc424aedd155cd7864f112e2faaa66c84a3
adriana maestre <adrianalamasbonita_15@hotmail.com>
2023-08-06 18:59:30 -0400

    Employee, SalaryEmployee & CommissionEmployee finish

---
+++ ConceptsPOO/ConceptsPOO/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace ConceptsPOO
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Date Birthdate { get; set; }
+
+        public Date Hiringdate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}," +
+                $" Birth: {Birthdate}, " +
+                $"Hiring: {Hiringdate}, " +
+                $"Is active: {IsActive}";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -1,17 +1,27 @@
-﻿
-
-using ConceptsPOO;
+﻿using ConceptsPOO;
 
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
-try
+Employee employee1 = new SalaryEmployee()
 {
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(2009, 9, 23));
-	Console.WriteLine(new Date(2008, 11, 31));
-
-}
-catch (Exception error)
-{
-    Console.WriteLine(error.Message);
-}
+    Id = 1010, 
+    FirstName = "Sandra", 
+    LastName = "Morales",
+    Birthdate = new Date(1990, 5, 23),
+    Hiringdate = new Date(2021, 3, 10),
+    IsActive = true,
+    Salary = 1000.00M
+};
+Console.WriteLine(employee1);
+Employee employee2 = new CommissionEmployee()
+{     
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    Birthdate = new Date(1995, 5, 23),
+    Hiringdate = new Date(2021, 2, 5),
+    IsActive = true,
+    Sales = 20000M,
+    CommissionPercentaje = 0.06F
+};
+Console.WriteLine(employee2);
